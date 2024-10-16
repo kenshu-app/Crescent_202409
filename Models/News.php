7fe7f49271a790850f70c83fb7468e6cb4527a1e
kenshu <kenshu.app@gmail.com>
2024-10-16 10:02:04 +0900

    fix:01-4~5 新着情報

--- Models/News.php
+++ Models/News.php
@@ -16,14 +16,23 @@ class News extends DB
     /**
      * ニュースの全件を返す
      *
+     * @param string|null $desc
+     * @param integer|null $offset
+     * @param integer|null $limit
      * @return array
      */
-    public function all(): array
+    public function all(?string $desc = null, ?int $offset = 0, ?int $limit = 0): array
     {
         try {
             $sql = 'SELECT';
             $sql .= ' *';
             $sql .= ' FROM ' . $this->tblMain;
+            if ($desc) {
+                $sql .= ' ORDER BY posted_at DESC, id DESC';
+            }
+            if ($limit > 0) {
+                $sql .= ' LIMIT ' . $offset . ', ' .  $limit;
+            }
             return $this->pdoObj->query($sql)->fetchAll();
         } catch (PDOException $e) {
             header('Content-Type: text/plain; charset=UTF-8', true, 500);
